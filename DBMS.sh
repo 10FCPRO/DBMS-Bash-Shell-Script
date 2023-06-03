f158ff84977133b1a0aa3834b1bbb20cc9fb47ea
HagarMohamedAnwar <120135534+HagarMohamedAnwar@users.noreply.github.com>
2023-06-03 18:21:22 +0300

    Add files via upload

--- DBMS.sh
+++ DBMS.sh
@@ -144,3 +144,94 @@ function selectCol {
   awk 'BEGIN{FS="|"}{print $'$colNum'}' $tName
   selectMenu
 }
+
+
+function selectCon {
+  echo -e "\n\n+--------Select Under Condition Menu-----------+"
+  echo "| 1. Select All Columns Matching Condition    |"
+  echo "| 2. Select Specific Column Matching Condition|"
+  echo "| 3. Back To Selection Menu                   |"
+  echo "| 4. Back To Main Menu                        |"
+  echo "| 5. Exit                                     |"
+  echo "+---------------------------------------------+"
+  echo -e "Enter Choice: \c"
+  read ch
+  case $ch in
+    1) clear; allCond ;;
+    2) clear; specCond ;;
+    3) clear; selectCon ;;
+    4) clear; cd ../.. 2>>./.error.log; mainMenu ;;
+    5) exit ;;
+    *) echo " Wrong Choice " ; selectCon;
+  esac
+}
+
+function allCond {
+  echo -e "Select all columns from TABLE Where FIELD(OPERATOR)VALUE \n"
+  echo -e "Enter Table Name: \c"
+  read tName
+  echo -e "Enter required FIELD name: \c"
+  read field
+  fid=$(awk 'BEGIN{FS="|"}{if(NR==1){for(i=1;i<=NF;i++){if($i=="'$field'") print i}}}' $tName)
+  if [[ $fid == "" ]]
+  then
+    echo "Not Found"
+    selectCon
+  else
+    echo -e "\nSupported Operators: [==, !=, >, <, >=, <=] \nSelect OPERATOR: \c"
+    read op
+    if [[ $op == "==" ]] || [[ $op == "!=" ]] || [[ $op == ">" ]] || [[ $op == "<" ]] || [[ $op == ">=" ]] || [[ $op == "<=" ]]
+    then
+      echo -e "\nEnter required VALUE: \c"
+      read val
+      res=$(awk 'BEGIN{FS="|"}{if ($'$fid$op$val') print $0}' $tName 2>>./.error.log |  column -t -s '|')
+      if [[ $res == "" ]]
+      then
+        echo "Value Not Found"
+        selectCon
+      else
+        awk 'BEGIN{FS="|"}{if ($'$fid$op$val') print $0}' $tName 2>>./.error.log |  column -t -s '|'
+        selectCon
+      fi
+    else
+      echo "Unsupported Operator\n"
+      selectCon
+    fi
+  fi
+}
+
+function specCond {
+  echo -e "Select specific column from TABLE Where FIELD(OPERATOR)VALUE \n"
+  echo -e "Enter Table Name: \c"
+  read tName
+  echo -e "Enter required FIELD name: \c"
+  read field
+  fid=$(awk 'BEGIN{FS="|"}{if(NR==1){for(i=1;i<=NF;i++){if($i=="'$field'") print i}}}' $tName)
+  if [[ $fid == "" ]]
+  then
+    echo "Not Found"
+    selectCon
+  else
+    echo -e "\nSupported Operators: [==, !=, >, <, >=, <=] \nSelect OPERATOR: \c"
+    read op
+    if [[ $op == "==" ]] || [[ $op == "!=" ]] || [[ $op == ">" ]] || [[ $op == "<" ]] || [[ $op == ">=" ]] || [[ $op == "<=" ]]
+    then
+      echo -e "\nEnter required VALUE: \c"
+      read val
+      res=$(awk 'BEGIN{FS="|"; ORS="\n"}{if ($'$fid$op$val') print $'$fid'}' $tName 2>>./.error.log |  column -t -s '|')
+      if [[ $res == "" ]]
+      then
+        echo "Value Not Found"
+        selectCon
+      else
+        awk 'BEGIN{FS="|"; ORS="\n"}{if ($'$fid$op$val') print $'$fid'}' $tName 2>>./.error.log |  column -t -s '|'
+        selectCon
+      fi
+    else
+      echo "Unsupported Operator\n"
+      selectCon
+    fi
+  fi
+}
+
+mainMenu
